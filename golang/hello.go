2bebc27681c631f39d16f8cbd5d1a3b84a256b8d
Jake Jennings <jakeajennings@protonmail.com>
2022-04-26 17:07:07 -0400

    update hello.go

--- golang/hello.go
+++ golang/hello.go
@@ -2,181 +2,132 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"sync"
+	"reflect"
+	"errors"
+	"math"
 )
 
 func main() {
-	//goroutine1()	//Will run both goroutines concurrently to completion 
-	//goroutine2()	//Will run both goroutines until main finishes
-	//goroutine3() 	//Will run both goroutines until user presses enter
-	//goroutine4()	//Will run a goroutine until the waitGroup is done
-	//goroutine5()	//Will run a goroutine until the msg is received over the channel
-	//goroutine6()	//Will run a goroutine until the channel is closed
-	//goroutine7()	//Will run a goroutine until the channel is closed
-	//goroutine8()	//Will run a goroutine until the channel has delivered two messages
-	//goroutine9()	//Will run a goroutine until the channel has delivered two messages
-	goroutine10()	//Will run a goroutine until the first 100 fibonacci are delivered
-}
+	// Hello World!
+	var hello string = "Hello World"
+	fmt.Println(hello)
 
-func goroutine1() {
-	go count("sheep") 
-	count("fish")	
-}
+	// Variables
+	var x int = 5
+	y := 11 //Shorthand for variable declaration
+	sum := sum(x, y)
+	var b bool = true
+	var gpa float64 = 3.6
+	var rune1 rune = 'B'
 
-func goroutine2() {
-	go count("sheep") 
-	go count("fish")	
-}
+	// Displaying rune and its type
+    fmt.Printf("Rune 1: %c; Unicode: %U; Type: %s \n", rune1, rune1, reflect.TypeOf(rune1))
+      
+	fmt.Printf("Sum = %d \n", sum)
 
-func goroutine3() {
-	go count("sheep") 
-	go count("fish")
-	fmt.Scanln()
-}
-
-func goroutine4() {
-	var waitGroup sync.WaitGroup
-	waitGroup.Add(1) //essentially a counter, incremented by the number of goroutines in the group
-	go func () { //using an anonymous wrapper func
-		count("sheep") 
-		waitGroup.Done()
-	}()
-
-	waitGroup.Wait() //will block until counter is zero
-}
-
-func goroutine5() {
-	c := make(chan string) //Channels are useful to communicate across different goroutines 
-	go countChannel("sheep", c)
-
-	msg := <- c // msg will receive the output of the channel (Note: this is a blocking function)
-	fmt.Println(msg)
-}
-
-func goroutine6() {
-	c := make(chan string) //Channels are useful to communicate across different goroutines 
-	go countChannel("sheep", c)
-
-	for {
-		msg, open := <- c  // msg will receive all output from the channel in a loop
-		if !open { //the loop will break when the channel is no longer open
-			break
-		}
-		fmt.Println(msg)
+	// if statements
+	if sum > 10 {
+		fmt.Println("Sum is greater than 10")
+	} else if sum == 10 {
+		fmt.Println("Sum is equal to 10")
+	} else {
+		fmt.Println("Sum is less than 10")		
 	}
-}
-
-func goroutine7() {
-	c := make(chan string) //Channels are useful to communicate across different goroutines 
-	go countChannel("sheep", c)
-
-	for msg := range c { // msg will receive all output from the channel in a loop
-		fmt.Println(msg)
-	} //the loop will break automatically when the channel is no longer open
-}
-
-func goroutine8() {
-	c := make(chan string, 2) //Note: the second param is the number of inputs to be carried across the channel before closing
-	c <- "hello"
-	c <- "world"
-	//c <- "third message" //If a 3rd message is added to the channel of size 2, then deadlock error will occur
-
-	msg := <- c
-	fmt.Println(msg)
-
-	msg = <- c
-	fmt.Println(msg)
-}
-
-func goroutine9() {
-	fast := make(chan string) 
-	slow := make(chan string) 
-
-	go func() {
-		for {
-			fast <- "Every 500ms"
-			time.Sleep(time.Millisecond * 500)
-		}
-	}()
-
-	go func() {
-		for {
-			slow <- "Every 2 seconds"
-			time.Sleep(time.Second * 2)
-		}
-	}()
-
-	for i := 0; i < 5; i++{
-		fmt.Println(<- fast) 
-		fmt.Println(<- slow) //Because reading from the channel is a blocking call, the output will only once the slow channel has received it's input
+	if b {
+		fmt.Printf("True! GPA = %f \n", gpa)
 	}
 
-	for {
-		select { //With a select statement, Println will not be blocked and outputs will be run whenever the channel has received it's input
-		case msg1 := <- fast:
-			fmt.Println(msg1) 
-		case msg2 := <- slow:
-			fmt.Println(msg2) 
-		}
+	//Arrays
+	var array [5]int //array length is fixed to it's type
+	array[2] = 88
+	fmt.Println(array)
+
+	//Slices
+	slice := []string{"This", "is", "a", "slice"} // Initialized as []T or []T{} or  []T{value1, value2, value3, ...value n}
+	slice = append(slice, "!!") //slice has dyanmic sizing, so append can be used to add elements at the end of the slice
+	fmt.Println(slice)
+	fmt.Printf("Length of the slice: %d \n", len(slice))
+
+	//Maps
+	vertices := make(map[string]int) // Initialized as map[key]value
+	vertices["triangle"] = 3
+	vertices["square"] = 4
+	vertices["hexagon"] = 6
+	fmt.Println(vertices)
+	delete(vertices, "triangle")
+	fmt.Println(vertices)
+	fmt.Printf("Vertices in a square: %d \n", vertices["square"])
+
+	//Loops
+	for i:= 0; i < 5; i++ { //for loop
+		fmt.Println(i)
 	}
+
+	i := 0
+	for i < 5 { //while loop
+		fmt.Println(i)
+		i++
+	}
+
+	for index, value := range slice { //loop over an array
+		fmt.Println("index:", index, "value:", value)
+	}
+
+	for key, value := range vertices { //loop over a map
+		fmt.Println("key:", key, "value:", value)
+	}
+
+	//Function call
+	result, err := sqrt(-16)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+
+	//Using struct
+	p := person{name: "Jimmy", age:40}
+	fmt.Println(p)
+	fmt.Printf("Age: %d \n", p.age)
+
+	//Pointers
+	fmt.Println("Pointer:", &i)
+	i = 7
+	inc_PassByValue(i)
+	fmt.Println(i)
+	inc_PassByReference(&i)
+	fmt.Println(i)
+}
+
+//Functions in the format
+//func functionName(param type...) returnType {}
+func sum(x int, y int) int {
+	return x + y
+}
+
+func sqrt(x float64) (float64, error) { //multiple return types are allowed
+	if x < 0 {
+		return 0, errors.New("Cannot calculate sqrt of negative numbers")
+	}
+	return math.Sqrt(x), nil //In Go, nil is the zero value for pointers, interfaces, maps, slices, channels and function types, representing an uninitialized value.
+}
+
+func inc_PassByValue (x int) {
+	x++
+}
+
+func inc_PassByReference (x int) {
+	*x++
+}
+
+//Struct
+type person struct {
+	name string
+	age int
+
 }
 
 
-func goroutine10() {
-	jobs := make(chan int, 100) //buffered channel
-	results := make(chan int, 100)
-
-	go worker(jobs, results) //this call can be duplicated to utilize a multi-core processor
-
-	for i := 0; i < 100; i++ {
-		jobs <- i
-	}
-	close(jobs)
-
-	for j := 0; j < 100; j++ {
-		fmt.Println(<-results) //TODO: Fix integer overflow on fib numbers larger than int max
-	}
-
-}
-
-//<-chan are channels that are only ever receiving
-//chan<- are channels that are only ever sending 
-func worker(jobs <-chan int, results chan<- int){
-	fib_map := make(map[int]int) // Initialized as map[key]value
-
-	for n := range jobs {
-		results <- fib(n, fib_map)
-	}0
-}
-
-func fib(n int, fib_map map[int]int) int {
-	nth_fib, exists := fib_map[n]
-    if exists {
-        return nth_fib
-    } else {
-		if n <= 1 {
-			fib_map[n] = n
-			return n
-		}
-		fib_map[n] = fib(n - 1, fib_map) + fib(n - 2, fib_map)
-		return fib_map[n]
-	}
-}
-
-func count(thing string) {
-	for i:= 0; i < 5; i++ {
-		fmt.Println(i, thing)
-		time.Sleep(time.Millisecond * 500)
-	}
-}
-
-func countChannel(thing string, c chan string) {
-	for i:= 0; i < 5; i++ {
-		c <- thing //send the string value through the channel
-		time.Sleep(time.Millisecond * 500)
-	}
-	close(c)
-}
 
 
